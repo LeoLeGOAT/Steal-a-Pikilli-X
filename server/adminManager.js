47e6fd41a5130962f4551bb36cf76cea631a5f75
LeoLeGOAT <leo.legoat@outlook.fr>
2026-03-21 15:08:21 +0100

    Update adminManager.js

--- server/adminManager.js
+++ server/adminManager.js
@@ -1,1 +1,20 @@
+class AdminManager {
+    constructor() {
+        this.admins = {};
+        this.code = "170713";
+    }
 
+    login(id, code) {
+        if (code === this.code) {
+            this.admins[id] = true;
+            return true;
+        }
+        return false;
+    }
+
+    isAdmin(id) {
+        return this.admins[id] === true;
+    }
+}
+
+module.exports = AdminManager;
