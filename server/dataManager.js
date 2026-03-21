d9f9b3f1f357388b94c34dd1b17bd50ce48d84d0
LeoLeGOAT <leo.legoat@outlook.fr>
2026-03-21 15:09:54 +0100

    Update dataManager.js

--- server/dataManager.js
+++ server/dataManager.js
@@ -1,1 +1,31 @@
+const fs = require("fs");
 
+class DataManager {
+    constructor() {
+        this.file = "./data/players.json";
+        if (!fs.existsSync(this.file)) {
+            fs.writeFileSync(this.file, JSON.stringify({}));
+        }
+    }
+
+    loadAll() {
+        return JSON.parse(fs.readFileSync(this.file));
+    }
+
+    saveAll(data) {
+        fs.writeFileSync(this.file, JSON.stringify(data, null, 2));
+    }
+
+    loadPlayer(id) {
+        const data = this.loadAll();
+        return data[id];
+    }
+
+    savePlayer(id, player) {
+        const data = this.loadAll();
+        data[id] = player;
+        this.saveAll(data);
+    }
+}
+
+module.exports = DataManager;
