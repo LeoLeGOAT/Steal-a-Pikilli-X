5d1e7b6b434511f50c32b284ef0d475caeea04de
LeoLeGOAT <leo.legoat@outlook.fr>
2026-03-21 15:07:58 +0100

    Update brainrotManager.js

--- server/brainrotManager.js
+++ server/brainrotManager.js
@@ -1,1 +1,13 @@
+class BrainrotManager {
+    spawn(player, data) {
+        const brainrot = {
+            name: data.name,
+            mutation: data.mutation || "none",
+            trait: data.trait || "none"
+        };
 
+        player.brainrots.push(brainrot);
+    }
+}
+
+module.exports = BrainrotManager;
